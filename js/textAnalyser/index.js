a22915c84b3ce3b5227c7df9b411cca6ddbbebbe
Mehmet Dogan <info@intelligentworks.ch>
2017-10-25 21:48:53 +0200

    create the alg. steps

--- js/textAnalyser/index.js
+++ js/textAnalyser/index.js
@@ -1,2 +1,33 @@
+$("button")
+    .on("click", function () {
+        // handle the click event
+    });
 
 
+/*
+jQuery
+1) Register an event handler on click event
+2) Get values (text, letters)
+
+Javascript
+3) Find occurences in the given text for the given letters
+4) Save all the information into a suitable data structure
+
+Jquery
+5) Based on the collected information, create the bar chart.
+*/
+
+
+function clickHandler() {
+
+}
+
+function getInputValues() {
+
+}
+
+function findOccurences() {
+
+}
+
+function createBarChart() {}
